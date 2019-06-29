9438e132190064ca4223619349de1493800d5bb2
Jabril Breckenridge <jabriltb2@gmail.com>
2019-06-29 14:58:45 -0500

    Cards mostly completed. Working on tab components.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -28,6 +28,7 @@ export default class Content extends Component {
 
 	changeSelected = tab => {
 		// this function should take in the tab and update the state with the new tab.
+		this.setState({ selected: this.tabs[tab] });
 	};
 
 	filterCards = () => {
@@ -43,6 +44,16 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+
+    if(this.state.selected === "all"){
+      return this.state.cards
+    } else {
+      this.state.cards.filter(card => {
+        if(card.tab === this.state.selected){
+          this.setState({card})
+        }
+      })
+    }
 		return this.state.cards;
 	};
 
@@ -54,7 +65,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-				<Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+
+        />
 				<Cards cards={this.filterCards()} />
 			</div>
 		);
